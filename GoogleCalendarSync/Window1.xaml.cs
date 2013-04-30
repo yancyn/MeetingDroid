577a3c45a0cf38f5222af23e608a9f43bb57391f
yancyn <yancyn@gmail.com>
2013-04-30 14:26:17 +0800

    Proof of concept retrieving event from Google Calendar with api v3.

---
+++ GoogleCalendarSync/Window1.xaml.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: yeang-shing.then
+ * Date: 4/30/2013
+ * Time: 10:00 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+using DotNetOpenAuth.OAuth2;
+using Google.Apis.Authentication;
+using Google.Apis.Authentication.OAuth2;
+using Google.Apis.Authentication.OAuth2.DotNetOpenAuth;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Samples.Helper;
+using Google.Apis.Services;
+using Google.Apis.Util;
+
+namespace GoogleCalendarSync
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+		
+		public void DisplayEvents()
+        {
+			try
+			{
+	            var provider = new NativeApplicationClient(GoogleAuthenticationServer.Description)
+				{
+					ClientIdentifier = ClientCredentials.ClientID,
+					ClientSecret = ClientCredentials.ClientSecret
+				};
+				var auth = new OAuth2Authenticator<NativeApplicationClient>(provider, GetAuthorization);
+				CalendarService service = new CalendarService(
+					new BaseClientService.Initializer(){Authenticator = auth}
+				);
+				
+				// @see http://stackoverflow.com/questions/8537681/google-api-v3-for-dotnet-using-the-calendar-with-an-api-key
+//				foreach(CalendarListEntry entry in service.CalendarList.List().Fetch().Items)
+//					System.Diagnostics.Debug.WriteLine(entry.Summary);
+				
+				//Calendar primary = service.Calendars.Get("").Fetch();
+				//System.Diagnostics.Debug.WriteLine(primary.Summary);
+				
+				//Event e = service.Events.Get("","s5ibdoum1jk6omru23dg1is8pc").Fetch();
+				//System.Diagnostics.Debug.WriteLine(e.Summary);
+				
+				EventLists.Items.Clear();
+				EventLists.ItemsSource = service.Events.List("").Fetch().Items;
+//				foreach(Event i in service.Events.List("").Fetch().Items)
+//				{
+//					i.Location, i.Start.DateTime, i.End, i.Attendees
+//					System.Diagnostics.Debug.WriteLine(i.Summary);
+//				}
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+				throw ex;
+			}
+        }
+		//private Expander CreateUIEventList(Event e, 
+
+		/// <summary>
+		/// TODO: Obtain cache session.
+		/// </summary>
+		/// <param name="client"></param>
+		/// <returns></returns>
+        private static IAuthorizationState GetAuthorization(NativeApplicationClient client)
+        {
+            // You should use a more secure way of storing the key here as
+            // .NET applications can be disassembled using a reflection tool.
+            //const string STORAGE = "google.calendars";
+            //const string KEY = "x{,erdzf11x9;89";
+            string scope = CalendarService.Scopes.Calendar.GetStringValue();
+
+            // Check if there is a cached refresh token available.
+//            IAuthorizationState state = AuthorizationMgr.GetCachedRefreshToken(STORAGE, KEY);
+//            if (state != null)
+//            {
+//                try
+//                {
+//                    client.RefreshToken(state);
+//                    return state; // Yes - we are done.
+//                }
+//                catch (DotNetOpenAuth.Messaging.ProtocolException ex)
+//                {
+//                    CommandLine.WriteError("Using existing refresh token failed: " + ex.Message);
+//                }
+//            }
+
+            // Retrieve the authorization from the user.
+            IAuthorizationState state = AuthorizationMgr.RequestNativeAuthorization(client, scope);
+            //AuthorizationMgr.SetCachedRefreshToken(STORAGE, KEY, state);
+            return state;
+        }
+		
+		void window1_Initialized(object sender, EventArgs e)
+		{
+			DisplayEvents();
+		}
+	}
+}
---
+++ Muje.Calendar.Test/GoogleCalendarTest.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: yeang-shing.then
+ * Date: 4/29/2013
+ * Time: 3:07 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+
+using DotNetOpenAuth.OAuth2;
+using Google.Apis.Authentication;
+using Google.Apis.Authentication.OAuth2;
+using Google.Apis.Authentication.OAuth2.DotNetOpenAuth;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util;
+
+using NUnit.Framework;
+
+namespace Muje.Calendar.Test
+{
+	/// <summary>
+	/// Description of GoogleCalendarTest.
+	/// </summary>
+	[TestFixture]
+	public class GoogleCalendarTest
+	{
+		private static IAuthorizationState GetAuthentication(NativeApplicationClient arg)
+  		{
+	      // Get the auth URL:
+	      IAuthorizationState state = new AuthorizationState(new[] { CalendarService.Scopes.Calendar.GetStringValue() });
+	      state.Callback = new Uri(NativeApplicationClient.OutOfBandCallbackUrl);
+	      Uri authUri = arg.RequestUserAuthorization(state);
+	
+	      // Request authorization from the user (by opening a browser window):
+	      Process.Start(authUri.ToString());
+	      Console.Write("  Authorization Code: ");
+	      string authCode = Console.ReadLine();
+	      Console.WriteLine();
+	
+	      // Retrieve the access token by using the authorization code:
+	      return arg.ProcessUserAuthorization(authCode, state);
+	  	}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <seealso cref="">https://developers.google.com/google-apps/calendar/instantiate</seealso>
+		/// <seealso cref="">http://code.google.com/p/google-api-dotnet-client/wiki/OAuth2</seealso>
+		/// <seealso cref="">https://developers.google.com/resources/api-libraries/documentation/calendar/v3/csharp/latest/index.html</seealso>
+		/// <seealso cref="">http://googleappsdeveloper.blogspot.com/2011/11/introducing-next-version-of-google.html</seealso>
+		[Test]
+		public void GetCalendarTest()
+		{
+			var provider = new NativeApplicationClient(GoogleAuthenticationServer.Description)
+			{
+				ClientIdentifier = "",
+				ClientSecret = ""
+			};
+			var auth = new OAuth2Authenticator<NativeApplicationClient>(provider, GetAuthentication);
+			CalendarService service = new CalendarService(
+				new BaseClientService.Initializer(){Authenticator = auth}
+			);
+			
+			// @see http://stackoverflow.com/questions/8537681/google-api-v3-for-dotnet-using-the-calendar-with-an-api-key
+			Google.Apis.Calendar.v3.CalendarListResource.ListRequest listRequest = service.CalendarList.List();
+			foreach(CalendarListEntry entry in listRequest.Fetch().Items)
+				System.Diagnostics.Debug.WriteLine(entry.Summary);
+			
+			//System.Diagnostics.Debug.WriteLine(service.Events.Get("","").EventId);
+			//service.Events.Get("","").BeginFetchAsStream
+//			StreamReader reader = new StreamReader(stream);
+//			string line = "";
+//			while(line != null)
+//			{
+//				line = reader.ReadLine();
+//				System.Diagnostics.Debug.WriteLine(line);
+//			}
+			//System.Diagnostics.Debug.WriteLine(e.Description);
+			//CalendarsResource calendars = service.Calendars;
+		}
+		
+		[Test]
+		public void CreateEventTest()
+		{
+		}
+	}
+}
