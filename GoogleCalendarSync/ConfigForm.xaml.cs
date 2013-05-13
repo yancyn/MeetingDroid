3efcd516e54cd2ba756f0c6af4ca3d5700503088
yancyn <yancyn@gmail.com>
2013-05-13 11:42:17 +0800

    Draft setting form.

---
+++ GoogleCalendarSync/ConfigForm.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: yeang-shing.then
+ * Date: 5/13/2013
+ * Time: 11:04 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace GoogleCalendarSync
+{
+	/// <summary>
+	/// Interaction logic for ConfigForm.xaml
+	/// </summary>
+	public partial class ConfigForm : Window
+	{
+		public ConfigForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
